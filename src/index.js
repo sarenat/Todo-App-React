c966d18072a3104c8b4634a1531b6d5556b1d66a
Sarena Tran <sirenasarena@sarenas-mbp.home>
2019-02-19 16:37:03 -0500

    implemented multiple lists

--- src/index.js
+++ src/index.js
@@ -2,32 +2,55 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+const ListItem = (props) => (
+  <li
+    onClick={props.onClick}
+    style={{textDecoration: props.isComplete ? "line-through" : "initial"}}
+  > {props.task} </li>
+)
+
+// class List extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   /* App will pass taskList and view. */
+//
+// }
+
 class App extends React.Component {
   state = {
-    taskList: [],
+    taskList: [[]],
     inputValue: "",
-    view: 0
+    view: 0,
+    listNum: 0
   }
 
   addTodo = () => {
     if (this.state.inputValue.trim()) {
       let updatedTaskList = this.state.taskList.slice();
-      updatedTaskList.push({
+      updatedTaskList[this.state.listNum].push({
         task: this.state.inputValue,
-        isComplete: false
+        isComplete: false,
+        itemListNum: this.state.listNum
       });
       this.setState({taskList: updatedTaskList, inputValue: ""});
     }
   }
 
-  checked = (index) => {
+  checked = (itemListNum, index) => {
     let updatedTaskList = this.state.taskList.slice();
-    updatedTaskList[index].isComplete = !updatedTaskList[index].isComplete;
+    updatedTaskList[itemListNum][index].isComplete = !updatedTaskList[itemListNum][index].isComplete;
     this.setState({taskList: updatedTaskList});
   }
 
-  updateView = (viewNum) => {
-    this.setState({view: viewNum});
+  updateView = (viewNum) => { this.setState({view: viewNum}); }
+
+  newList = () => {
+    let updatedTaskList = this.state.taskList.slice();
+    updatedTaskList.push([]);
+
+    let newListNum = this.state.listNum+1;
+    this.setState({listNum: newListNum, taskList: updatedTaskList});
   }
 
   render() {
@@ -48,15 +71,17 @@ class App extends React.Component {
           }}
         />
         <button onClick={this.addTodo}>Add Task</button>
+        <button onClick={this.newList}>New List</button>
 
         <ul>
-          {/* I tried putting this section into it's own function. Why didn't it work? */
-            this.state.taskList.map((todo, index) => {
-              var returnValue = <List
+          {/* I tried putting this section into it's own function. Why didn't it work? */}
+          {this.state.taskList[0].map((todo, index) => {
+              var returnValue = <ListItem
                                   key={index}
                                   task={todo.task}
                                   isComplete={todo.isComplete}
-                                  onClick={() => {this.checked(index)}}
+                                  itemListNum={todo.itemListNum}
+                                  onClick={() => {this.checked(todo.itemListNum, index)}}
                                 />
               if(this.state.view === 0) {
                 return returnValue
@@ -72,15 +97,6 @@ class App extends React.Component {
     );
   }
 }
-
-/* This function is declared outside of the class because it doesn't change anything
- * that has to do with the class App. Place filtering here. */
-const List = (props) => (
-  <li
-    onClick={props.onClick}
-    style={{textDecoration: props.isComplete ? "line-through" : "initial"}}
-  > {props.task} </li>
-)
 
 // ============================================================================
 
